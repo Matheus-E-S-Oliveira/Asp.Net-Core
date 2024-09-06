a581fa9521cd1bd23d1c5606e92c81e13f9b81da
Matheus Oliveira <marheuseric@gmail.com>
2024-09-06 10:09:05 -0300

    Criando View para exibir tarefas

--- GerenciadorTarefas/obj/Debug/net8.0/GerenciadorTarefas.AssemblyInfo.cs
+++ GerenciadorTarefas/obj/Debug/net8.0/GerenciadorTarefas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GerenciadorTarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72c365efc76e715f335f18f243d64225a55b2c76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42221ddbf5e31c2e53e289d2e7a73bf1f0ce3838")]
 [assembly: System.Reflection.AssemblyProductAttribute("GerenciadorTarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GerenciadorTarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
