18cff943c853c30ab7907e46aec91fec26daa1b7
Matheus Oliveira <marheuseric@gmail.com>
2024-09-09 10:50:45 -0300

    Atualização

--- GerenciadorTarefas/Program.cs
+++ GerenciadorTarefas/Program.cs
@@ -21,6 +21,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Tarefa}/{action=Index}/{id?}");
 
 app.Run();
